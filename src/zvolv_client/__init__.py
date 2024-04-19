7bfec9e309bc817cd9b047ddaed95d468b2aa3e6
Akshay Jadhav <akshay@zvolv.com>
2024-04-19 12:45:02 +0530

    code cleanup

--- src/zvolv_client/__init__.py
+++
@@ -1,8 +0,0 @@
-import requests
-from auth import Auth
-
-class ZvolvClient:
-    def __init__(self, base_url):
-        self.base_url = base_url
-        self.session = requests.Session()
-        self.auth = Auth(self.session, self.base_url)
---
+++ tests/test_2.py
@@ -0,0 +1,9 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+	def test(self):
+		self.assertEqual(4, 4)
+
+if __name__ == '__main__':
+    unittest.main()
