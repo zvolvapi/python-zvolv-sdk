7a3fc58e01c01da06c204acee3ef47aea0bac23a
Akshay Jadhav <akshay@zvolv.com>
2024-06-08 23:55:00 +0530

    Pydantic type casting support for forms module

--- zvolv_sdk/modules/forms.py
+++ zvolv_sdk/modules/forms.py
@@ -1,9 +1,30 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 import requests
 from pydantic import BaseModel
 
+class Element(BaseModel):
+    elementId: str
+    label: str
+    type: str
+    defaultValue: Optional[Union[str, bool, int, dict, list]] = None
+    required: Optional[bool] = True
+    disabled: Optional[bool] = True
+    hidden: Optional[bool] = True
+    unique: Optional[bool] = True
+    updateIfUnique: Optional[bool] = True
+    properties: Optional[dict] = {}
+    attributes: Optional[dict] = {}
+    dependencies: Optional[List] = []
+    validations: Optional[List] = []
+    dataType: str
+    rbacConf: Optional[dict] = {}
+    abacConf: Optional[dict] = {}
+    showLabel: Optional[bool] = True
+    id: str
+
 class Form(BaseModel):
     id: str
+    uuid: str
     title: str
     description: Optional[str] = None
     icon: str
@@ -20,53 +41,8 @@ class Form(BaseModel):
     tags: Optional[List] = []
     canReadRoles: Optional[List] = []
     canUpdateRoles: Optional[List] = []
-    elements: Optional[List] = []
-
-
-#     {
-  
-#   "elements": [
-#     {
-#       "elementId": "string",
-#       "label": "string",
-#       "type": "EDIT_TEXT",
-#       "defaultValue": {},
-#       "required": true,
-#       "disabled": true,
-#       "hidden": true,
-#       "unique": true,
-#       "updateIfUnique": true,
-#       "properties": {},
-#       "attributes": {},
-#       "dependencies": {},
-#       "validations": {},
-#       "dataType": "INT",
-#       "rbacConf": {
-#         "canReadElementRoles": [
-#           0
-#         ],
-#         "canUpdateElementRoles": [
-#           0
-#         ]
-#       },
-#       "abacConf": {
-#         "canReadSubmissionAttributes": [
-#           "string"
-#         ],
-#         "canUpdateSubmissionAttributes": [
-#           "string"
-#         ]
-#       },
-#       "showLabel": true,
-#       "id": "string"
-#     }
-#   ],
-  
-#   "configurations": {},
- 
-# }
-    
-
+    configurations: Optional[dict] = {}
+    elements: List[Element] = []
 
 class Forms:
     def __init__(self, session, base_url):
@@ -75,12 +51,12 @@ class Forms:
         self.workspace_instance = None
     
     def get(self, id):
-        """Get form details from id."""
+        """Get form details from id"""
         url = f"{self.base_url}/api/v1/forms/{id}"
         response = self.session.get(url)
         if response.status_code == 200:
             resp = response.json()
-            if resp.get('error') == False:
+            if resp.get('error') == False and resp['data']['elements']:
                 return Form(**resp['data']['elements'][0])
             else:
                 print("Form get Failed")
@@ -88,16 +64,30 @@ class Forms:
 
         return response.json()
     
-    def put(self, id):
-        """Get form details from id."""
-        url = f"{self.base_url}/api/v1/forms/{id}"
-        response = self.session.put(url)
+    def put(self, form: Form, enableRetrofit: bool = False, enableReSync: bool = False):
+        """Update existing form"""
+        url = f"{self.base_url}/api/v1/forms?enableRetrofit={enableRetrofit}&enableReSync={enableReSync}"
+        response = self.session.put(url, json=form.model_dump())
         if response.status_code == 200:
             resp = response.json()
             if resp.get('error') == False:
-                return resp['data']
+                return resp
             else:
                 print("Form put Failed")
+                print(response.json())
+
+        return response.json()
+    
+    def post(self, form: Form):
+        """Create a new form"""
+        url = f"{self.base_url}/api/v1/forms"
+        response = self.session.post(url, json=form.model_dump())
+        if response.status_code == 200:
+            resp = response.json()
+            if resp.get('error') == False:
+                return resp
+            else:
+                print("Form post Failed")
                 print(response.json())
 
         return response.json()
