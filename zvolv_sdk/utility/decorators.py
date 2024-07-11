f4bb05f3e85ae0984109eaa8538e1d392636dcd1
Akshay Jadhav <akshay@zvolv.com>
2024-07-11 13:28:18 +0530

    status log decorator added

--- zvolv_sdk/utility/decorators.py
+++ zvolv_sdk/utility/decorators.py
@@ -12,3 +12,18 @@ def enforce_pydantic_model(model_class):
             return func(*args, **kwargs)
         return wrapper
     return decorator
+
+def attach_status_log(func):
+    def wrapper(*args, **kwargs):
+        context = args[0]
+        event = args[1]
+        automation_uuid = event['headers']['x-nuclio-function-name']
+        context.client.logger.initExecutionLog(automation_uuid)
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            context.client.logger.closeExecutionLog('failed', str(e), None)
+            raise
+        finally:
+            context.client.logger.closeExecutionLog('success', 'Execution completed', None)
+    return wrapper
